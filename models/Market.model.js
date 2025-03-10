4510e916ffdf81e0ad8b1aaa5ec550b97f6df332
Johana Castellano <johanacg16@gmail.com>
2025-03-10 17:10:43 +0000

    market model updated

--- models/Market.model.js
+++ models/Market.model.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require("mongoose");
 
+const DAYS_OF_WEEK = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
 const marketSchema = new Schema(
   {
     name: {
@@ -9,21 +11,27 @@ const marketSchema = new Schema(
     },
     image: {
       type: String,
-      /* default: ".png" */
+      default: "https://images.unsplash.com/photo-1533900298318-6b8da08a523e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     location: {
-        type: String
+      type: String
     },
-    timetable: {
+    schedule: [{
+      days: {
+        type: [String],
+        enum: DAYS_OF_WEEK
+      },
+      hours: {
         type: String
-    },
+      }
+    }],
     description: {
       type: String
     },
     town: {
-        type: Schema.Types.ObjectId,
-        ref: 'Town',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: 'Town',
+      required: true
     }
   }
 );
